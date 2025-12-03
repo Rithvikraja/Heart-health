e50d4552646f1c07f8ea5bb19ee2fe7f4a3eb3e0
Rithvikraja <150163251+Rithvikraja@users.noreply.github.com>
2025-12-03 12:17:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -173,6 +173,7 @@ force_high_risk = (
 # Apply override
 if force_high_risk:
     res = 1
+    
         if age > 50:
             reason_lines.append("Your age increases overall cardiovascular sensitivity.")
         if chol > 220:
@@ -384,6 +385,7 @@ if st.button("Calculate Total Cholesterol"):
     """, unsafe_allow_html=True)
 
 st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
